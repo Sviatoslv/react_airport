31ede182e103d72db85a76e7b3dcd1c02dc6eec5
Пользователь <Sviatoslv>
2019-07-22 20:19:47 +0300

    Updates

---
+++ precache-manifest.cd1d717d1e7ff40122456b6c2c779b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df59a4a8166e330781a6c90c743a9f9",
+    "url": "/react_airport/index.html"
+  },
+  {
+    "revision": "e8f83e180c087f5dc6e7",
+    "url": "/react_airport/static/css/main.480174bd.chunk.css"
+  },
+  {
+    "revision": "9be7f90bf2062767907d",
+    "url": "/react_airport/static/js/2.f0452fdd.chunk.js"
+  },
+  {
+    "revision": "e8f83e180c087f5dc6e7",
+    "url": "/react_airport/static/js/main.7e2ade8b.chunk.js"
+  },
+  {
+    "revision": "c8cd8e70b1c2d1cd9765",
+    "url": "/react_airport/static/js/runtime~main.037fa8df.js"
+  },
+  {
+    "revision": "4a4dc51359c99266479375af141e3081",
+    "url": "/react_airport/static/media/refresh.4a4dc513.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_airport/precache-manifest.2b1cc2bfbee41366a0270597566a8204.js"
+  "/react_airport/precache-manifest.cd1d717d1e7ff40122456b6c2c779b38.js"
 );
 
 self.addEventListener('message', (event) => {
